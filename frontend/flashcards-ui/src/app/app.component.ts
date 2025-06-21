93df50b3d48ecb52a424f6109891d03daca2c082
shai bachar <shaibachar@gmail.com>
2025-06-21 21:20:53 +0300

    to save

--- frontend/flashcards-ui/src/app/app.component.ts
+++ frontend/flashcards-ui/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { AuthService } from './services/auth.service';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, NgIf, MenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'flashcards-ui';
+  menuOpen = false;
   constructor(public auth: AuthService, private router: Router) {}
 
   get userAvatarUrl(): string {
@@ -25,5 +27,13 @@ export class AppComponent {
     } else {
       this.router.navigate(['/login']);
     }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 }
